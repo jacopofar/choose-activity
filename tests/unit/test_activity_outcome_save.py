a100a791293fb41674f24df8a6aa990a52c92ecf
Jacopo Farina <jacopo.farina@flixbus.com>
2019-10-24 16:20:44 +0200

    Add test for the number of lines in outcome log

--- tests/unit/test_activity_outcome_save.py
+++ tests/unit/test_activity_outcome_save.py
@@ -6,15 +6,18 @@ from choose_activity.helpers import ActivityOutcome, log_activity_result
 
 def test_log_outcome(tmp_path):
     test_path = tmp_path / 'activities_state.log'
-    log_activity_result(test_path, ActivityOutcome(
-        activity='勉強',
-        start_at=datetime.now().astimezone(),
-        end_at=datetime.now().astimezone() + timedelta(minutes=42),
-        is_done=True,
-        feedback='''some
-           multiline
-                thing!
+    for l in range(5):
+        log_activity_result(test_path, ActivityOutcome(
+            activity='勉強',
+            start_at=datetime.now().astimezone(),
+            end_at=datetime.now().astimezone() + timedelta(minutes=42),
+            is_done=True,
+            feedback='''some
+            multiline
+                    thing!
 
-                 😬 😬 😬
-                 ''',
-    ))
+                    😬 😬 😬
+                    ''',
+        ))
+        content = open(test_path).read()
+        assert len(content.split('\n')) == l + 1
