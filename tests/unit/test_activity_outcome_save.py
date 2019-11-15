cb21649fcc7030a56eec264397908ecbeb6453a8
Jacopo Farina <jacopo.farina@flixbus.com>
2019-11-15 18:01:43 +0100

    Add test for latest_outcome_for_activity

--- tests/unit/test_activity_outcome_save.py
+++ tests/unit/test_activity_outcome_save.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta
 
-from choose_activity.helpers import ActivityOutcome, log_activity_result
+from choose_activity.helpers import (
+    ActivityOutcome,
+    log_activity_result,
+    latest_outcome_for_activity
+)
 
 
 def test_log_outcome(tmp_path):
@@ -22,3 +26,19 @@ def test_log_outcome(tmp_path):
         lines = content.split('\n')
         assert lines[-1] == ''
         assert len(lines) == l + 2  # 0-indexed + empty line
+
+
+def test_get_latest_outcome(tmp_path):
+    test_path = tmp_path / 'activities_state.log'
+    for l in range(5):
+        log_activity_result(test_path, ActivityOutcome(
+            activity=f'activity type #{l}',
+            start_at=datetime.now().astimezone(),
+            end_at=datetime.now().astimezone() + timedelta(minutes=42),
+            is_done=True,
+            feedback=f'activity description {l}!',
+        ))
+
+    assert (
+        latest_outcome_for_activity(test_path, 'activity type #1') ==
+        'activity description 1!')
